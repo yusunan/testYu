94298191d93ec5dd58f100127745d744b6e379b9
yusunan <yusunan8@163.com>
2018-08-12 23:28:42 +0800

    fix log

--- framework/log.py
+++ framework/log.py
@@ -1,17 +1,22 @@
 # -*- coding: UTF-8 -*-
 # 基础包：日志服务
 import logging
+import os
 import time
 
 def get_logger():
-    global log_path
-    try:
-        log_path
-    except NameError:
-        log_path = "/data/log/apiTest/"
-    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    # file = tezLogPath + time.strftime("%Y-%m-%d", time.localtime()) + ".log"
-    # logging.basicConfig(filename=file, level=logging.INFO, format=FORMAT)
-    # 开发阶段为了方便调试，可不输出到文件
-    logging.basicConfig(level=logging.INFO, format=FORMAT)
-    return logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level=logging.INFO)
+    if not logger.handlers:
+        log_filename = time.strftime("%Y-%m-%d-%H", time.localtime()) + '.log'
+        handler = logging.FileHandler(os.path.join("log/", log_filename))
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
+
+        logger.addHandler(handler)
+        logger.addHandler(console)
+    return logger
